d4ac286baea8fee2286cf373d0383a91f0fb817f
ElliotGamer3 <elliotimhoff@gmail.com>
2022-07-07 17:40:33 -0500

    Updates to add debug.js and more buttons for the MapEditor. made marker.js more generic in nature so that export of the feature object can be done in a looping way.

--- Features/Marker/js/marker.js
+++ Features/Marker/js/marker.js
@@ -1,21 +1,54 @@
 //The marker feature class used to create a marker and add it to the map : Elliot 7/5/2022
 class FeatureMarker {
     //id needs to default to a new id but be able to be set to a specific id : Elliot 7/6/2022
-    constructor(latlng, options, id = Date.now()) {
+    //objectParameters is an object with the following properties:
+    //  - latlng: L.LatLng
+    //  - options: L.Marker.Options
+    constructor(objectParameters, id = Date.now()) {
+        debugInfo("FeatureMarker constructor");
+        this.marker = L.marker(
+            objectParameters.latlng || [0, 0], //default to 0,0 if no latlng is provided : Elliot 7/7/2022
+            objectParameters.options || {} //default to an empty object if no options are provided : Elliot 7/7/2022
+        );
+        debugInfo("FeatureMarker constructor: marker created");
+        debugInfo(this.marker);
         this.exportDetails = {
-            id: id,
-            type: "Marker",
+            id: id, //always present in any features exportDetails : Elliot 7/7/2022
+            type: "Marker", //always present in any features exportDetails, contents will vary : Elliot 7/7/2022
+            //always present in any features exportDetails, contents will vary: Elliot 7/7/2022
             parameters: {
-                latlng: latlng,
-                options: options,
+                latlng: objectParameters.latlng,
+                options: objectParameters.options,
             },
+            object: this.marker, //always present in any features exportDetails : Elliot 7/7/2022
         };
-        this.marker = L.marker(latlng, options);
+        debugInfo("FeatureMarker constructor: exportDetails created");
+        debugInfo(this.exportDetails);
     }
-    //Adds a marker to the Map that will be exported using the ExportedMap utility : Elliot 7/6/2022
-    addToExport() {}
+    //returns the object that will be exported: Elliot 7/6/2022
+    getExport() {
+        return this.exportDetails;
+    }
+
+    //returns the object can be added to a map : Elliot 7/7/2022
+    getObject() {
+        return this.exportDetails.object;
+    }
+
     //returns the Marker that can be added to the map : Elliot 7/6/2022
     getMarker() {
         return this.marker;
+    }
+
+    //latlng is a L.latlng type
+    //sets the latlng in the exportDetails : Elliot 7/7/2022
+    //and updates the marker :
+    setLatLng(latlng) {
+        this.exportDetails.parameters.latlng = latlng;
+    }
+
+    //sets the options in the exportDetails : Elliot 7/7/2022
+    setOptions(options) {
+        this.exportDetails.parameters.options = options; //set the option to the new options : Elliot 7/7/2022
     }
 }
--- Pages/MapEditor/js/ButtonCallbacks.js
+++ Pages/MapEditor/js/ButtonCallbacks.js
@@ -1,4 +1,14 @@
-//The callback function for the newMarker button : Elliot 7/6/2022
+//The callback function for the newMarker button Map Editor : Elliot 7/6/2022
 function newMarker(e) {
     MAP_EDITOR_ON_CLICK_STATE = "ADD_MARKER";
+}
+
+//The callback function for the popupEditer button in Map Editor : Elliot 7/7/2022
+function popupEditor(e) {
+    MAP_EDITOR_ON_CLICK_STATE = "POPUP_EDITOR";
+}
+
+//The callback function for the none button in Map Editor : Elliot 7/7/2022
+function none(e) {
+    MAP_EDITOR_ON_CLICK_STATE = "NONE";
 }
