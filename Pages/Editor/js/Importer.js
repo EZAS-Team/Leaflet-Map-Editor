a04406799bb3367e09903f41cf245626a12c639c
Dank-Fish <66405058+Dank-Fish@users.noreply.github.com>
2022-11-10 16:26:59 -0600

    added csv parser

--- Pages/Editor/js/Importer.js
+++ Pages/Editor/js/Importer.js
@@ -6,8 +6,46 @@ document.addEventListener("importTheMap", (e) => {
     importMap();
 });
 
-//function to convert csv into an array
-function csvToArray(str, delimiter = ",") {
+function parseCSV(str) {
+    var arr = [];
+    var quote = false;  // 'true' means we're inside a quoted field
+
+    // Iterate over each character, keep track of current row and column (of the returned array)
+    for (var row = 0, col = 0, c = 0; c < str.length; c++) {
+        var cc = str[c], nc = str[c+1];        // Current character, next character
+        arr[row] = arr[row] || [];             // Create a new row if necessary
+        arr[row][col] = arr[row][col] || '';   // Create a new column (start with empty string) if necessary
+
+        if (cc == '\r' && nc == '\n' && !quote) { ++row; col = 0; ++c; continue; } if (cc == '\n' && !quote) { ++row; col = 0; continue; }
+
+        // If the current character is a quotation mark, and we're inside a
+        // quoted field, and the next character is also a quotation mark,
+        // add a quotation mark to the current column and skip the next character
+        if (cc == '"' && quote && nc == '"') { arr[row][col] += cc; ++c; continue; }
+
+        // If it's just one quotation mark, begin/end quoted field
+        if (cc == '"') { quote = !quote; continue; }
+
+        // If it's a comma and we're not in a quoted field, move on to the next column
+        if (cc == ',' && !quote) { ++col; continue; }
+
+        // If it's a newline (CRLF) and we're not in a quoted field, skip the next character
+        // and move on to the next row and move to column 0 of that new row
+        if (cc == '\r' && nc == '\n' && !quote) { ++row; col = 0; ++c; continue; }
+
+        // If it's a newline (LF or CR) and we're not in a quoted field,
+        // move on to the next row and move to column 0 of that new row
+        if (cc == '\n' && !quote) { ++row; col = 0; continue; }
+        if (cc == '\r' && !quote) { ++row; col = 0; continue; }
+
+        // Otherwise, append the current character to the current column
+        arr[row][col] += cc;
+    }
+    return arr;
+}
+
+//function to convert csv into an map
+function csvToMap(str, delimiter = ",") {
 
     // slice from start of text to the first \n index
     // use split to create an array from string by delimiter
@@ -163,7 +201,7 @@ function importMap() {
         var reader = new FileReader();
         reader.onload = function (e){
             var text = e.target.result;
-            var data = csvToArray(text);
+            var data = csvToMap(text);
             JSON.stringify(data);
 
             for (var i in data){
